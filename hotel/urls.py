28d877b97bbe0bbae4c14697da7c610ab1a1aeed
kush1acc <92995145+kush1acc@users.noreply.github.com>
2022-09-18 22:27:21 +0530

    Update urls.py

--- hotel/urls.py
+++ hotel/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('/', views.Home,name="index"),
+    path('', views.Home,name="index"),
     path('contact/', views.contact),
     path('contactus/', views.contactus),
     path('names/',views.names),
